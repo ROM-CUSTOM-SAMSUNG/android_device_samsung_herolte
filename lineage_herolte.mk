e1e6ab258a816e13b1e3a95ddcde73760313da33
debie | a3y17ltexc <79523414+debie-A320FL@users.noreply.github.com>
2022-04-09 12:40:11 +0200

    Spoof pixel build fingerprint to pass safynet
    Fingerprint must match security patch in order to pass safynet. (here march 2022 build fingerprint)
    You can pick a google device fingerprint from nippon gsi : https://t.me/nippongsi

--- lineage_herolte.mk
+++ lineage_herolte.mk
@@ -39,4 +39,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=heroltexx \
     PRIVATE_BUILD_DESC="heroltexx-user 8.0.0 R16NW G930FXXU2ERD5 release-keys"
 
-BUILD_FINGERPRINT := samsung/heroltexx/herolte:8.0.0/R16NW/G930FXXU2ERD5:user/release-keys
+BUILD_FINGERPRINT := "google/raven/raven:12/S3B1.220218.006/8325196:user/release-keys"
